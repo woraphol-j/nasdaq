e6b5ee5f2d3c1162c1579f13b41f0e3df894791d
woraphol <s4510488@gmail.com>
2016-07-22 01:00:18 +0700

    Add EBS

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
@@ -22,7 +21,6 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
